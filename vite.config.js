3ce87c31be53f9c087f271c8e9b6d161ffa2094d
technoidteam <sahandulmith@gmail.com>
2025-01-28 12:16:24 +0530

    new pub

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
-  },
-};
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
